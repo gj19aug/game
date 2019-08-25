35951fde7c13ea86286f110936328f238c8a06f5
Adam Byrd <adam1.byrd@gmail.com>
2019-08-25 16:30:29 -0700

    Fix previous commit

---
+++ game/Assets/Utility/Types.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using UnityEngine;
+
+public class MinMaxRangeAttribute : PropertyAttribute
+{
+    public float min;
+    public float max;
+
+    public MinMaxRangeAttribute(float min, float max)
+    {
+        this.min = min;
+        this.max = max;
+    }
+}
+
+[Serializable]
+public struct Range
+{
+    public float min;
+    public float max;
+}
+
+[Serializable]
+public struct IntRange
+{
+    public int min;
+    public int max;
+}
