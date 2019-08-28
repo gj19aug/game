b32f48193de799dbbf52f0f5eef2b44a51a8ac6b
Chris Havranek <chrishavranek@gmail.com>
2019-08-27 20:03:27 -0700

    Pause menu's restart button

---
+++ game/Assets/Menus/RestartButton.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class RestartButton : MonoBehaviour
+{
+
+    public void RestartGame()
+    {
+        SceneManager.LoadScene(0, LoadSceneMode.Single);
+
+    }
+
+
+    
+}
